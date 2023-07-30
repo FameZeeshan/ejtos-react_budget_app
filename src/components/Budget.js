6c245bcd8a23485aab6a7c56c95b86db1fd220b7
FameZeeshan <zeeshan6143@gmail.com>
2023-07-30 22:45:04 +0530

    complete

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,17 @@
+// Budget.js
 
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+const Budget = () => {
+  const { budget, currency } = useContext(AppContext);
+  return (
+    <div className="alert alert-secondary">
+      <span>
+        {currency} {budget}
+      </span>
+    </div>
+  );
+};
+
+export default Budget;
